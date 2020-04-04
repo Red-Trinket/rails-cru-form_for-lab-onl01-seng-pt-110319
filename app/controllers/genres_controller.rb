37e23d58908e1fd656fe87c144bf8a54a0639259
Matthew Jones <matts3371@gmail.com>
2020-04-04 15:10:28 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,37 +1,29 @@
-class GenresController < ApplicationController 
-  
-  def index 
-    @genres = Genre.all 
-  end 
-  
-  def show 
+class GenresController < ApplicationController
+  def show
     @genre = Genre.find(params[:id])
-  end 
-  
-  def new 
-    @genre = Genre.new 
-  end 
-  
-  def create 
-    @genre= Genre.new(genre_params)
-    @genre.save
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params)
     redirect_to genre_path(@genre)
-  end 
-  
-  def edit 
+  end
+
+  def edit
     @genre = Genre.find(params[:id])
-  end 
-  
-  def update 
+  end
+
+  def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
     redirect_to genre_path(@genre)
-  end 
-  
-  private 
-  
+  end
+
+  private
   def genre_params
     params.require(:genre).permit(:name)
-  end 
-  
-end 
+  end
+end
